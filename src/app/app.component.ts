57958a6ef1811309bdd887ab9a91b4d7ff2a5ec9
Akilesh-02 <akileshshekar21@gmail.com>
2022-07-01 15:34:51 +0530

    ngFor, ngIf , User model and Input passed from selector directive @Input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from './user/user.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  user: User;
+
+  constructor() {
+    this.user = new User();
+    this.user.name = 'Akilesh';
+    this.user.company = 'Capgemini';
+    this.user.phoneNumbers = ['97434', '81232'];
+  }
 }
